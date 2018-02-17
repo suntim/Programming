4ce5ca0b8f04427357471fcd9a88271315279505
Alex_XT <1056294025@qq.com>
2018-02-17 05:28:26 +0000

    Update trainLossPlot.py

--- trainLossPlot.py
+++ trainLossPlot.py
@@ -7,12 +7,14 @@ import re
 from mpl_toolkits.axes_grid1 import host_subplot
 
 # read the log file
-fp = open('faster_rcnn_end2end_ResNet-50-model_.txt', 'r')
+fp = open('logs/fast_rcnn_ohem_VGG16_2016-07-21_19-56-33.txt', 'r')
 
 train_iterations = []
 train_loss = []
-test_iterations = []
-# test_accuracy = []
+loss_bbox = []
+loss_cls = []
+rpn_cls_loss = []
+rpn_loss_bbox = []
 
 for ln in fp:
     # get train_iterations and train_loss
@@ -20,6 +22,15 @@ for ln in fp:
         arr = re.findall(r'ion \b\d+\b,', ln)
         train_iterations.append(int(arr[0].strip(',')[4:]))
         train_loss.append(float(ln.strip().split(' = ')[-1]))
+    if 'Train net output #0: ' in ln and 'loss_bbox = ' in ln:
+        loss_bbox.append(float(ln.strip().split(' = ')[-1].split(' ')[0]))
+    if 'Train net output #1: ' in ln and 'loss_cls = ' in ln:
+        loss_cls.append(float(ln.strip().split(' = ')[-1].split(' ')[0]))
+    if 'Train net output #2: ' in ln and 'rpn_cls_loss = ' in ln:
+        rpn_cls_loss.append(float(ln.strip().split(' = ')[-1].split(' ')[0]))
+    if 'Train net output #3: ' in ln and 'rpn_loss_bbox = ' in ln:
+        rpn_loss_bbox.append(float(ln.strip().split(' = ')[-1].split(' ')[0]))
+
 
 fp.close()
 
@@ -29,11 +40,9 @@ plt.subplots_adjust(right=0.8)  # ajust the right boundary of the plot window
 # set labels
 host.set_xlabel("iterations")
 host.set_ylabel("loss")
-# par1.set_ylabel("validation accuracy")
 
 # plot curves
 p1, = host.plot(train_iterations, train_loss, 'b-',label="train loss")
-# p2, = par1.plot(test_iterations, test_accuracy, label="validation accuracy")
 
 # set location of the legend,
 # 1->rightup corner, 2->leftup corner, 3->leftdown corner
@@ -42,10 +51,42 @@ host.legend(loc=1)
 
 # set label color
 host.axis["left"].label.set_color(p1.get_color())
-# par1.axis["right"].label.set_color(p2.get_color())
-# set the range of x axis of host and y axis of par1
 host.set_xlim([train_iterations[0], train_iterations[-1]+5])
 host.set_ylim([train_loss[-1], train_loss[0]])
 
+fig2 = plt.figure("fig2_loss_bbox")
+plt.plot(train_iterations, loss_bbox, 'b-',label="loss_bbox")
+plt.legend(loc='upper right')
+plt.xlabel("iterations")
+plt.ylabel("loss_bbox")
+
+fig3 = plt.figure("fig3_loss_cls")
+plt.plot(train_iterations, loss_cls, 'm-',label="loss_cls")
+plt.legend(loc='upper right')
+plt.xlabel("iterations")
+plt.ylabel("loss_cls")
+
 plt.draw()
 plt.show()
+fig2.show()
+fig3.show()
+
+if not rpn_cls_loss == []:
+    fig4 = plt.figure("fig4_rpn_cls_loss")
+    plt.plot(train_iterations, rpn_cls_loss, 'c-*',label="rpn_cls_loss")
+    plt.legend(loc='upper right')
+    plt.xlabel("iterations")
+    plt.ylabel("rpn_cls_loss")
+    fig4.show()
+if not rpn_loss_bbox == []:
+    fig5 = plt.figure("fig5_rpn_loss_bbox")
+    plt.plot(train_iterations, rpn_loss_bbox, 'k-^',label="rpn_loss_bbox")
+    plt.legend(loc='upper right')
+    plt.xlabel("iterations")
+    plt.ylabel("rpn_loss_bbox")
+    fig5.show()
+
+
+
+
+
